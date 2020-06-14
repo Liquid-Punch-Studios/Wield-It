bd2ff412574a5df1ed62c037d539e410e6636e3a
Ahmet Sait <git@ahmetsait.com>
2020-06-14 15:45:45 +0300

    Add sample scene
    - Post processing
    - Materials
    - Template scripts
    - Added gruond layer

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 shoulderOffset;  /// This is the point hand & weapon rotates against
+    public float armLength = 1.5f;  /// How much further weapon can be held from shoulder
+
+    public float wieldFactor = 0.1f; /// How much the hand moves in relation to the mouse/touch delta
+    // TODO: Maybe seperate mouse & touch?
+
+    public float moveSpeed = 10;
+    public float jumpSpeed = 10;
+    public float airMoveSpeed = 4;
+    public float airJumpSpeed = 8;
+    // TODO: Decide if control percent for air is better
+    public int airJumpCount = 1;
+    private int airJumpLeft;
+    private int onGround = 0;
+
+    public LayerMask groundLayerMask; // Doesn't 
+
+    private Rigidbody2D playerRb;
+    private Rigidbody2D weaponRb;
+    private RelativeJoint2D weaponJoint;
+
+    // This controls whether weapon is held in a fixed angle
+    // Usually used when stabbing enemies and terrain, also 
+    // makes some cool air slam possible
+    private bool fixedWeaponAngle;
+
+    private float realAngularOffset; // Need to keep track of angle because relative joint resets after [180, -180]
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // FixedUpdate is called once per physics frame
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    public void Wield(Vector2 delta)
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if ((collision.gameObject.layer | groundLayerMask.value) != 0)
+            onGround++;
+        if (onGround > 0)
+            airJumpLeft = airJumpCount;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if ((collision.gameObject.layer | groundLayerMask.value) != 0)
+            onGround--;
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(RelativeJoint2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Weapon : MonoBehaviour
+{
+	public Vector2 weaponHandle;
+	public float Damage;
+	public float speedTreshold;
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.blue;
+		Vector3 handle = this.transform.TransformPoint(new Vector3(weaponHandle.x, weaponHandle.y));
+		Gizmos.DrawSphere(handle, 0.1f);
+	}
+}
