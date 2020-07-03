cd5c09958e79716a531258f389fc8304b78de5ec
MErenAydin <merenep12@gmail.com>
2020-07-03 14:53:23 +0300

    Added music and sound settings at settings menu

--- Assets/Scripts/SettingsData.cs
+++ Assets/Scripts/SettingsData.cs
@@ -9,7 +9,9 @@ public class SettingsData
     public int difficultyVal;
     public bool violence;
     public int soundVal;
+    public bool soundMuted;
     public int musicVal;
+    public bool musicMuted;
 
     public SettingsData(MainMenuSelection menu)
     {
@@ -17,6 +19,8 @@ public class SettingsData
         difficultyVal = menu.Difficulty;
         violence = menu.Violence;
         soundVal = menu.SoundVal;
+        soundMuted = menu.SoundMuted;
         musicVal = menu.MusicVal;
+        musicMuted = menu.MusicMuted;
     }
 }
