2343ba7da374bc7c0cc4d74b53fd18c4ea663e74
MErenAydin <merenep12@gmail.com>
2020-07-04 02:22:48 +0300

    Added new traps' prefabs

---
+++ Assets/Scripts/Trap Scripts/DragonHead.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonHead : MonoBehaviour
+{
+    public ParticleSystem fireParticleSystem;
+
+
+    public float playTime;
+    public float pauseTime;
+
+    private float playTimer;
+    private float pauseTimer;
+
+    private bool playing = false;
+
+    private void Start()
+    {
+        pauseTimer = pauseTime;
+        playTimer = playTime;
+    }
+
+    void Update()
+    {
+        float now = Time.deltaTime;
+        
+        if (playing)
+        {
+            if(!fireParticleSystem.isPlaying)
+                fireParticleSystem.Play(true);
+            playTimer -= now;
+            if (playTimer <= 0.0f)
+            {
+                playing = false;
+                playTimer = playTime;
+            }
+        }
+        else
+        {
+            if(fireParticleSystem.isPlaying)
+                fireParticleSystem.Stop(true);
+            pauseTimer -= now;
+            if (pauseTimer <= 0.0f)
+            {
+                playing = true;
+                pauseTimer = pauseTime;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Trap Scripts/FallingSpikes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikes : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.GetComponentInChildren<Rigidbody>().isKinematic = false;
+        Debug.Log(other.gameObject.name);
+    }
+}
