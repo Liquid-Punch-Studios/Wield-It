4ed389afe2ad35258e76b2c704029f75a72b914d
MErenAydin <merenep12@gmail.com>
2020-07-03 03:45:30 +0300

    Added saving player preferenced settings

---
+++ Assets/Scripts/SettingsData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SettingsData
+{
+    public int qualityVal;
+    public int difficultyVal;
+    public bool violence;
+    public int soundVal;
+    public int musicVal;
+
+    public SettingsData(MainMenuSelection menu)
+    {
+        qualityVal = menu.Quality;
+        difficultyVal = menu.Difficulty;
+        violence = menu.Violence;
+        soundVal = menu.SoundVal;
+        musicVal = menu.MusicVal;
+    }
+}
