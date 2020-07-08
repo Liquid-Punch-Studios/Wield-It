f12a734a5fae29daa7629e004c603a0e88047396
Ahmet Sait <git@ahmetsait.com>
2020-07-08 14:31:43 +0300

    Experimental camera turn ahead

---
+++ Assets/Scripts/TurnAhead.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TurnAhead : MonoBehaviour
+{
+	public Rigidbody followTarget;
+	public float turnFactor;
+	public float damping;
+	private float turnVelocity;
+
+	void Update()
+	{
+		var rot = transform.localRotation.eulerAngles;
+		rot.y = Mathf.SmoothDampAngle(rot.y, followTarget.velocity.x * turnFactor, ref turnVelocity, damping);
+		transform.localRotation = Quaternion.Euler(rot);
+	}
+}
