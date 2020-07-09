b774c6f09751cf8823ed951a2d4011de903388ea
MErenAydin <merenep12@gmail.com>
2020-07-09 14:27:28 +0300

    Added toggling platform

---
+++ Assets/Scripts/Trap Scripts/TogglingTrap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglingTrap : MonoBehaviour
+{
+    public float delay;
+    private Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponentInChildren<Animator>();
+        anim.enabled = false;
+        StartCoroutine(DelayedAnimation());
+    }
+
+    IEnumerator DelayedAnimation()
+    {
+        yield return new WaitForSeconds(delay);
+        anim.enabled = true;
+    }
+}
