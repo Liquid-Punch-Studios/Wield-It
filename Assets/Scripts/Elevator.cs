6606ff8a225a77fed6cb6f250ec054da7594f018
MErenAydin <merenep12@gmail.com>
2020-07-11 01:51:47 +0300

    Added Demo 3 scene and scene transitions

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Elevator : MonoBehaviour
+{
+	Animator anim;
+
+	public LayerMask triggerLayerMask;
+
+    private void Start()
+    {
+		anim = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+	{
+		int currentLevel = SceneManager.GetActiveScene().buildIndex;
+		if ((1 << other.gameObject.layer & triggerLayerMask.value) != 0)
+        {
+			anim.SetBool("isSet", true);
+			Level.NextLevel(currentLevel);
+			Debug.Log(currentLevel);
+			StartCoroutine(Level.LoadLevelDelayed(1f, currentLevel + 1));
+			SaveSystem.SaveLastLevel();
+		}
+	}
+
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Level
+{
+    public static int lastLevel = 1;
+    public static int levelCount = 3;
+
+    public static void NextLevel(int currentLevel)
+    {
+        if (currentLevel == lastLevel && lastLevel < levelCount)
+            lastLevel++;
+    }
+
+    public static IEnumerator LoadLevelDelayed(float delay, int level)
+    {
+        yield return new WaitForSeconds(delay);
+        if (level <= levelCount)
+            SceneManager.LoadSceneAsync(level, LoadSceneMode.Single);
+        else
+            SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+        yield return null;
+    }
+}
