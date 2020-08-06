8be81adb967df095319af3212331afb2dbd27fa7
Ahmet Sait <git@ahmetsait.com>
2020-08-06 16:53:14 +0300

    Revert Singleton stuff

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -8,7 +8,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
 	// Check to see if we're about to be destroyed.
 	private static bool shuttingDown = false;
-
+	private static object @lock = new object();
 	private static T instance;
 
 	/// <summary>
@@ -20,31 +20,42 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 		{
 			if (shuttingDown)
 			{
-				Debug.LogWarning($"[Singleton] Instance {typeof(T).Name} already destroyed. Returning null.");
+				Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
+					"' already destroyed. Returning null.");
 				return null;
 			}
 
-			return instance;
+			lock (@lock)
+			{
+				if (instance == null)
+				{
+					// Search for existing instance.
+					instance = (T)FindObjectOfType(typeof(T));
+
+					// Create new instance if one doesn't already exist.
+					if (instance == null)
+					{
+						// Need to create a new GameObject to attach the singleton to.
+						var singletonObject = new GameObject();
+						instance = singletonObject.AddComponent<T>();
+						singletonObject.name = typeof(T).ToString() + " (Singleton)";
+
+						// Make instance persistent.
+						DontDestroyOnLoad(singletonObject);
+					}
+				}
+
+				return instance;
+			}
 		}
 	}
 
-	private void Awake()
-	{
-		if (instance == null)
-		{
-			instance = this as T;
-			DontDestroyOnLoad(gameObject);
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
-	}
 
 	private void OnApplicationQuit()
 	{
 		shuttingDown = true;
 	}
+
 
 	private void OnDestroy()
 	{
