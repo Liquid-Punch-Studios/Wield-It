001ed2c31984332cc9a5fb0f8369a2032d07950a
MErenAydin <merenep12@gmail.com>
2020-08-07 16:11:30 +0300

    Added Music continuity

---
+++ Assets/Scripts/Persistence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistence : MonoBehaviour
+{
+	public static Persistence instance;
+	private void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+}
