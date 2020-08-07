6a1b831039380b0fe598f084a0a2d0df36ad783e
MErenAydin <merenep12@gmail.com>
2020-08-07 17:07:24 +0300

    Added enemy dead body

---
+++ Assets/Scripts/DungeonDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonDoor : MonoBehaviour
+{
+
+    public Health playerHealth;
+
+    Animator anim;
+    AudioSource audio;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        playerHealth.Died += PlayerHealth_Died;
+    }
+
+    private void PlayerHealth_Died(object sender, System.EventArgs e)
+    {
+        anim.SetBool("IsSet", true);
+        audio.Play();
+    }
+}
