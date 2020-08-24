117417814313fc139c211bc6f96825af335f07ae
Ahmet Sait <git@ahmetsait.com>
2020-08-24 16:50:51 +0300

    Add camera shake on damage received

---
+++ Assets/Scripts/PlayerHurtShake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerHurtShake : MonoBehaviour
+{
+	public Health health;
+	public float damageShakeThreshold = 10;
+
+	private CinemachineImpulseSource imp;
+
+	private void Start()
+	{
+		imp = GetComponent<CinemachineImpulseSource>();
+	}
+
+	private void OnEnable()
+	{
+		health.DamageReceived += Health_DamageReceived;
+	}
+
+	private void OnDisable()
+	{
+		health.DamageReceived -= Health_DamageReceived;
+	}
+
+	private void Health_DamageReceived(object sender, float e)
+	{
+		if (e >= damageShakeThreshold)
+			imp.GenerateImpulse();
+	}
+}
