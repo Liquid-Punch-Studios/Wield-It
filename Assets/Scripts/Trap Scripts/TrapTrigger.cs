f738868dc4e34bc2aa1ba97fd00d12fc16307291
MErenAydin <merenep12@gmail.com>
2020-08-28 01:57:41 +0300

    Trap stucks workaround

--- Assets/Scripts/Trap Scripts/TrapTrigger.cs
+++ Assets/Scripts/Trap Scripts/TrapTrigger.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class TrapTrigger : MonoBehaviour
 {
 	public Animator anim;
-
 	public LayerMask triggerLayerMask;
+	private Health health;
 
-	private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
 	{
+		if (other.TryGetComponent(out health))
+            health.Died += Health_Died;
 		if ((1 << other.gameObject.layer & triggerLayerMask.value) != 0)
 			anim.SetBool("isSet", true);
 	}
 
-	private void OnTriggerExit(Collider other)
+    private void Health_Died(object sender, System.EventArgs e)
+    {
+		anim.SetBool("isSet", false);
+	}
+
+    private void OnTriggerExit(Collider other)
 	{
 		if ((1 << other.gameObject.layer & triggerLayerMask.value) != 0)
 			anim.SetBool("isSet", false);
