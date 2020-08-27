a60245e9292b9477a4e31ee4aed539bb7e16b345
Ahmet Sait <git@ahmetsait.com>
2020-08-27 23:26:52 +0300

    Add impact shake for fist trap

--- Assets/Scripts/Trap Scripts/ImpactParticleTrigger.cs
+++ Assets/Scripts/Trap Scripts/ImpactParticleTrigger.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ImpactParticleTrigger : MonoBehaviour
 {
 	public ParticleSystem particle;
 	public AudioPlayer audioPlayer;
+	public CinemachineImpulseSource impulse;
 
 	public void Trigger()
 	{
 		particle.Play(true);
-		if(audioPlayer != null)
-			audioPlayer.PlayRandom(0.05f);
+		audioPlayer?.PlayRandom(0.05f);
+		impulse?.GenerateImpulse();
 	}
 }
