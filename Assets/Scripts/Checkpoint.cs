02570d1392b3cb897a86f9e119140122b349e340
MErenAydin <merenep12@gmail.com>
2020-08-30 14:38:07 +0300

    Added Checkpoin system

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private bool hasPassed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasPassed)
+            if(other.transform == GameManager.Instance.player.transform)
+            {
+                GameManager.Instance.RespawnPoint = transform.Find("RespawnPoint");
+                hasPassed = true;
+            }
+    }
+}
