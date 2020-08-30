a64cbb71c708bc9a19d9328b4290ee97dfd75337
MErenAydin <merenep12@gmail.com>
2020-08-30 19:45:42 +0300

    Checkpoint perfected

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     private bool hasPassed = false;
-
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!hasPassed)
             if(other.transform == GameManager.Instance.player.transform)
             {
+                animator.SetTrigger("isSet");
                 GameManager.Instance.RespawnPoint = transform.Find("RespawnPoint");
                 hasPassed = true;
             }
