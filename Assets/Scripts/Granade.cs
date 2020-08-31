2d57e48ca4f2e736ec9f102abc330262cd71cf02
Ahmet Sait <git@ahmetsait.com>
2020-09-01 01:26:55 +0300

    Grenade and breakable wooden ground changes
    - Perfected grenade animation
    - Better explosion impact calculation
    - Explosion range adjustment
    - Fixed camera shake distance of wooden ground and grenade
    - Clean up ThrownWeapon script a bit
    - Disable RoughMetal material lightmap
    - Added trail particles to thrown grenade

--- Assets/Scripts/Granade.cs
+++ Assets/Scripts/Granade.cs
@@ -6,93 +6,97 @@ using UnityEngine;
 
 public class Granade : MonoBehaviour
 {
-    public float timer;
-    public float deadLine = 3;
-    GameObject Explosion, damageIndicator;
-    CinemachineImpulseSource impulse;
-    void Start()
-    {
-        Explosion = Resources.Load<GameObject>("ExplosionEffect");
-        damageIndicator = (GameObject)Resources.Load("DamageIndicator");
-        impulse = GetComponent<CinemachineImpulseSource>();
-        StartCoroutine(Explode());
-    }
+	public float timer = 3;
 
-    public IEnumerator Explode()
-    {
-        yield return new WaitForSeconds(timer);
-        impulse.GenerateImpulse();
-        var ex = Instantiate(Explosion);
-        ex.transform.position = transform.position;
-        Harm();
-        Destroy(gameObject);
-        yield return null;
-    }
+	public float damage = 100;
+	public float force = 100;
+	public AnimationCurve damageFallof = AnimationCurve.Linear(0, 1, 1, 0);
 
-    public void Harm()
-    {
-        RaycastHit[] rcs = Physics.SphereCastAll(transform.position, 6, Vector3.up);
+	public float inRadius = 5;
+	public float outRadius = 5; // Additive
+	public float TotalRadius { get => inRadius + outRadius; }
 
-        foreach(RaycastHit rc in rcs)
-        {
-            if (rc.collider.attachedRigidbody == null)
-                continue;
-            var distanceToRaycast = (transform.position - rc.transform.position).magnitude;
-            if (rc.transform.TryGetComponent(out SlamBreak sb) && distanceToRaycast < 3.5f)
-            {
-                sb.Break();
-                continue;
-            }
+	public GameObject ExplosionEffect;
+	public GameObject damageIndicator;
+	public CinemachineImpulseSource impulse;
 
-            var hasHealth = rc.collider.attachedRigidbody.transform.TryGetComponent(out Health health);
-            var isPlayer = rc.transform == GameManager.Instance.player.transform;
+	public Animator animator;
+	public AnimationClip steamupClip;
 
-            if (!isPlayer)
-            {
-                if (hasHealth && distanceToRaycast < deadLine)
-                {
-                    health.ReceiveDamage(300);
-                    IndicateDamage(300, rc.collider.attachedRigidbody.transform.Find("DISpawn"));
-                }
-                else if (hasHealth)
-                {
-                    var damage = (150 / distanceToRaycast);
-                    if (rc.transform.gameObject != GameManager.Instance.player)
-                    {
-                        health.ReceiveDamage(damage);
-                        IndicateDamage(damage, rc.collider.attachedRigidbody.transform.Find("DISpawn"));
-                    }
-                        
-                }
-            }
+	void Start()
+	{
+		StartCoroutine(Explode());
+	}
 
-            var hasRigidbody = rc.transform.TryGetComponent(out Rigidbody rb);
+	public IEnumerator Explode()
+	{
+		yield return new WaitForSeconds(Mathf.Clamp(timer - steamupClip.length, 0, timer));
+		animator.SetTrigger("isSet");
+		yield return new WaitForSeconds(steamupClip.length);
+		impulse.GenerateImpulse();
+		Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+		Harm();
+		Destroy(gameObject);
+	}
 
-            if (hasRigidbody && distanceToRaycast < deadLine)
-                rb.AddExplosionForce(500, transform.position, 10, 0.5f, ForceMode.Impulse);
-            else if (hasRigidbody)
-            {
-                if (distanceToRaycast != 0)
-                    //TODO: Change values to make perfect explosion :)
-                    rb.AddExplosionForce(500 / distanceToRaycast, transform.position, 10, 0.5f, ForceMode.Impulse);
-            }
-        }
+	public void Harm()
+	{
+		RaycastHit[] rcs = Physics.SphereCastAll(transform.position, TotalRadius, Vector3.up);
 
-        void IndicateDamage(float damage, Transform DISpawn)
-        {
-            if (DISpawn == null)
-                return;
-            var di = Instantiate(damageIndicator);
-            if (damage > 0)
+		foreach (RaycastHit rc in rcs)
+		{
+			if (rc.collider.attachedRigidbody == null)
+				continue;
 
-                di.GetComponentInChildren<TextMeshPro>().text = "-" + (int)damage;
+			float distance = (transform.position - rc.transform.position).magnitude;
 
-            else
-            {
-                di.transform.Find("Block").gameObject.SetActive(true);
-                di.GetComponentInChildren<TextMeshPro>().gameObject.SetActive(false);
-            }
-            di.transform.position = DISpawn.transform.position;
-        }
-    }
+			Vector3 closestPoint = rc.collider.ClosestPointOnBounds(transform.position);
+			float closestDistance = (closestPoint - transform.position).magnitude;
+			
+			if (closestDistance < inRadius && rc.transform.TryGetComponent(out SlamBreak sb))
+			{
+				sb.Break();
+				continue;
+			}
+
+			float impact = damageFallof.Evaluate((Mathf.Clamp(closestDistance, inRadius, TotalRadius) - inRadius) / outRadius);
+
+			var hasHealth = rc.collider.attachedRigidbody.transform.TryGetComponent(out Health health);
+			var isPlayer = rc.transform == GameManager.Instance.player.transform;
+
+			if (!isPlayer && hasHealth && impact > 0)
+			{
+				if (health.ReceiveDamage(damage * impact))
+					IndicateDamage(damage * impact, rc.collider.attachedRigidbody.transform.Find("DISpawn"));
+			}
+
+			if (rc.transform.TryGetComponent(out Rigidbody rb))
+				rb.AddExplosionForce(force * impact, transform.position, TotalRadius, 1f, ForceMode.Impulse);
+		}
+	}
+
+	void IndicateDamage(float damage, Transform DISpawn)
+	{
+		if (DISpawn == null)
+			return;
+		var di = Instantiate(damageIndicator);
+		if (damage > 0)
+
+			di.GetComponentInChildren<TextMeshPro>().text = "-" + (int)damage;
+
+		else
+		{
+			di.transform.Find("Block").gameObject.SetActive(true);
+			di.GetComponentInChildren<TMP_Text>().gameObject.SetActive(false);
+		}
+		di.transform.position = DISpawn.transform.position;
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, inRadius);
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, TotalRadius);
+	}
 }
