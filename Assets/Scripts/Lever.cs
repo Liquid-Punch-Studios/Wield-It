d97433d3ebb8fca6571ee345c641c7186cb28743
Muhammet Eren Aydın <merenep12@gmail.com>
2022-04-13 12:24:37 -0400

    stuff after years

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public LayerMask triggerLayerMask;
+    public GameObject button;
+    private bool playerInCollider;
+    private Controls controls;
+
+    private Animator buttonAnim;
+    private Health health;
+    Animator anim;
+    AudioSource audioSrc;
+    void Start()
+    {
+        anim = GetComponentInChildren(typeof(Animator)) as Animator;
+        audioSrc = gameObject.GetComponent<AudioSource>();
+        controls = GameManager.Instance.controls;
+        buttonAnim = button.GetComponent<Animator>();
+        health = gameObject.GetComponent<Health>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInCollider)
+        {
+            if (controls.Player.Interaction.triggered)
+            {
+                button.SetActive(false);
+                anim.SetBool("isSet", true);
+                audioSrc.Play();
+                health.Hp -= health.MaxHp;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if ((1 << other.gameObject.layer & triggerLayerMask.value) != 0)
+        {
+            button.SetActive(true);
+            playerInCollider = true;
+            buttonAnim.SetBool("isSet", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if ((1 << other.gameObject.layer & triggerLayerMask.value) != 0)
+        {
+            button.SetActive(false);
+            playerInCollider = false;
+            buttonAnim.SetBool("isSet", false);
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponRack.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WeaponRack : MonoBehaviour
+{
+    public LayerMask triggerLayerMask;
+    public GameObject button;
+    public Sprite sprite;
+    public GameObject prefab;
+
+    private bool playerInCollider;
+    private Controls controls;
+    private Animator buttonAnim;
+    private RadialMenu radial;
+    private GameObject di;
+    private GameObject player;
+
+    public enum Weapons // your custom enumeration
+    {
+        NinjaStar,
+        Kunai,
+        Granade,
+        Spear
+    };
+    public Weapons weaponType = Weapons.Spear;
+
+    private void Start()
+    {
+        controls = GameManager.Instance.controls;
+        buttonAnim = button.GetComponent<Animator>();
+        radial = GameObject.Find("RadialMenu").GetComponent<RadialMenu>();
+        player = GameObject.Find("Player");
+        di = (GameObject)Resources.Load("Indicator");
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInCollider)
+        {
+            if (controls.Player.Interaction.triggered)
+            {
+                bool flag = false;
+                foreach(var item in radial.menu)
+                {
+                    if (item.name == weaponType.ToString())
+                    {
+                        if (item.maxAmount - item.Amount > 0)
+                            di.transform.Find("Text").GetComponent<TextMeshPro>().text = "+" + (item.maxAmount - item.Amount) + " " + weaponType.ToString();
+                        else
+                            di.transform.Find("Text").GetComponent<TextMeshPro>().text = "Inventory Full";
+                        Instantiate(di, player.transform.Find("DISpawn").transform.position, Quaternion.identity);
+                        item.Amount = item.maxAmount;
+                        flag = true;
+                    }
+                }
+
+                if (!flag)
+                {
+                    radial.AddWeapon(sprite, prefab, weaponType.ToString());
+                    di.transform.Find("Text").GetComponent<TextMeshPro>().text = weaponType.ToString() + " refilled";
+                    Instantiate(di, player.transform.Find("DISpawn").transform.position, Quaternion.identity);
+                }
+                radial.Reload();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if ((1 << other.gameObject.layer & triggerLayerMask.value) != 0)
+        {
+            button.SetActive(true);
+            playerInCollider = true;
+            buttonAnim.SetBool("isSet", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if ((1 << other.gameObject.layer & triggerLayerMask.value) != 0)
+        {
+            button.SetActive(false);
+            playerInCollider = false;
+            buttonAnim.SetBool("isSet", false);
+        }
+    }
+}
