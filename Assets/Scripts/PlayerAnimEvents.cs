f23b20a408c036c6a4f5b2783ba3acd0351b6279
Muhammet Eren Aydın <merenep12@gmail.com>
2022-08-16 03:09:13 -0400

    Player Movement State Machine

---
+++ Assets/Scripts/PlayerAnimEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour
+{
+    private PlayerController pc;
+    public void ToIdleState()
+    {
+        pc.MovementState = MovementActions.Idle;
+    }
+    void Start()
+    {
+        pc = transform.parent.GetComponent<PlayerController>();
+    }
+}
